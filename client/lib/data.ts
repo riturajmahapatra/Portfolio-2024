d034db9a725b2948a0d294b9807e4735f8a26bc2
Rituraj Mahapatra <riturajmahapatra@gmail.com>
2024-05-22 15:52:06 +0530

    assets added

--- client/lib/data.ts
+++ client/lib/data.ts
@@ -2,7 +2,10 @@ import React from 'react';
 import { CgWorkAlt } from 'react-icons/cg';
 import { FaReact } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
-import jansevaImg from '../src/assets/js-1.PNG';
+import jansevaImg from '../src/assets/js-1.png';
+import carWalaImg from '../src/assets/cw-1.png';
+import taxiImg from '../src/assets/tx-1.png';
+import countrynomicsImg from '../src/assets/cn-1.png';
 // import corpcommentImg from "@/public/corpcomment.png";
 // import rmtdevImg from "@/public/rmtdev.png";
 // import wordanalyticsImg from "@/public/wordanalytics.png";
@@ -74,14 +77,21 @@ export const projectsData = [
     description:
       'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
     tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    // imageUrl: rmtdevImg,
+    imageUrl: countrynomicsImg,
   },
   {
     title: 'Word Analytics',
     description:
       'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
     tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-    // imageUrl: wordanalyticsImg,
+    imageUrl: taxiImg,
+  },
+  {
+    title: 'Word Analytics',
+    description:
+      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+    imageUrl: carWalaImg,
   },
 ] as const;
 
