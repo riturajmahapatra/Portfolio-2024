d8ca345f932958758c004598796fa704ca1b9b1d
Rituraj Mahapatra <riturajmahapatra@gmail.com>
2024-05-26 15:40:43 +0530

    backend connection with DB

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+//port connection to the backend server
+const PORT = process.env.PORT || 5000;
+const URL = process.env.MONGO_URL;
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
+  })
+  .catch((error) => console.error('Error connecting to MongoDB: ', error));
